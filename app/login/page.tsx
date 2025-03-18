e8a559b655034d347fba41f246e61609dbbcf640
Vijay <vjwarboy13@gmail.com>
2025-03-18 08:44:57 +0530

    next auth google sso created

---
+++ app/login/page.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function LoginPage() {
+  const { data: session } = useSession();
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold mb-4">NextAuth.js with TypeScript</h1>
+      {session ? (
+        <>
+          <p>Welcome, {session.user?.name}</p>
+          <img src={session.user?.image || ""} alt="Profile" className="w-16 h-16 rounded-full mt-2" />
+          <button onClick={() => signOut()} className="mt-4 p-2 bg-red-500 text-white rounded">
+            Logout
+          </button>
+        </>
+      ) : (
+        <button onClick={() => signIn("google")} className="p-2 bg-blue-500 text-white rounded">
+          Login with Google
+        </button>
+      )}
+    </div>
+  );
+}
